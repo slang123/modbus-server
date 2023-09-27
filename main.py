67b322d49d85a053d2ab1cb98997ae00b85efdb8
Tummas Joensen <tummas@elektron.fo>
2023-09-27 23:36:24 +0100

    longer register and delay with 10sec

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import binascii
 import array
 import sys
 import os
+from time import sleep
 from _thread import *
 
 
-registers_input = [[0], [0], [0], [0], [0], [2301, 22, 16, 101, 102, 0, 0x5555, 0x6666, 4812], [2321, 25, 11, 120, 122, 0, 0x5555, 0x6666, 4802]]
-registers_holding = [[0], [1], [5600, 6000, 1], [6500], [6501], [2301, 101, 102, 4500, 4500, 560, 112, 34, 11], [2301, 101, 102, 4500, 4500, 560, 112, 23, 23]]
+registers_input = [[0], [0], [0], [0], [0], [2301, 22, 16, 101, 102, 0,
+                                             0x5555, 0x6666, 4812], [2321, 25, 11, 120, 122, 0, 0x5555, 0x6666, 4802]]
+registers_holding = [[0], [1], [5600, 6000, 1], [6500], [6501], [
+    2301, 150, 150, 4110, 4110, 560, 69, 34, 11, 15, 15, 15, 1, 1, 0, 15, 15, 15, 15], [2301, 150, 150, 4500, 4500, 560, 89, 23, 23, 15, 15, 15, 0, 0, 0, 15, 15, 15]]
 
 ServerSideSocket = socket.socket()
-host = '0.0.0.0'
-port = 5020
+host = '192.168.8.142'
+port = 502
 ThreadCount = 0
 try:
     ServerSideSocket.bind((host, port))
@@ -19,6 +22,7 @@ except socket.error as e:
     print(str(e))
 print('Socket is listening..')
 ServerSideSocket.listen(5)
+
 
 def modbus_crc16(data):
     crc = 0xFFFF
@@ -30,10 +34,11 @@ def modbus_crc16(data):
             crc = crc >> 1
             if carry_flag == 1:
                 crc = crc ^ 0xA001
-    return bytes([crc % 256,crc >> 8 % 256])
+    return bytes([crc % 256, crc >> 8 % 256])
+
 
 def multi_threaded_client(connection):
-    #connection.send(str.encode('Server is working:'))
+    # connection.send(str.encode('Server is working:'))
     while True:
         data = connection.recv(256)
         if not data:
@@ -43,13 +48,15 @@ def multi_threaded_client(connection):
         offs = (data[2] << 8) | data[3]
         nreg = (data[4] << 8) | data[5]
 
-        if id==7 :
-            txbytes = bytes([0, 0, 40 ,77 ,11, 4])
+        sleep(10)
+
+        if id == 7:
+            txbytes = bytes([0, 0, 40, 77, 11, 4])
             print("Returning error bytes:", txbytes)
             connection.send(txbytes)
             continue
-        if id==8 :
-            txbytes = bytes([0, 0, 11 ,77 ,11, 4, 7, 1, 2, 3, 255, 255])
+        if id == 8:
+            txbytes = bytes([0, 0, 11, 77, 11, 4, 7, 1, 2, 3, 255, 255])
             print("Returning error bytes:", txbytes)
             connection.send(txbytes)
             continue
@@ -59,20 +66,24 @@ def multi_threaded_client(connection):
             if (offs + nreg) > len(registers_input[id]):
                 txbytes = bytes([id, cmd+0x80, 0x02])
                 crc = modbus_crc16(txbytes)
-                txbytes = txbytes + crc[0].to_bytes(1,'big')+crc[1].to_bytes(1,'big')
+                txbytes = txbytes + \
+                    crc[0].to_bytes(1, 'big')+crc[1].to_bytes(1, 'big')
                 print(txbytes)
                 connection.send(txbytes)
             else:
                 txbytes = bytes([id, cmd, nreg*2])
                 # add the data to the package
-                n=0
+                n = 0
                 while n < nreg:
                     bytes_val = registers_input[id][n+offs].to_bytes(2, 'big')
-                    txbytes = txbytes + bytes_val[0].to_bytes(1,'big') + bytes_val[1].to_bytes(1,'big')
-                    n=n+1
+                    txbytes = txbytes + \
+                        bytes_val[0].to_bytes(
+                            1, 'big') + bytes_val[1].to_bytes(1, 'big')
+                    n = n+1
                 # Add crc to bytes
                 crc = modbus_crc16(txbytes)
-                txbytes = txbytes + crc[0].to_bytes(1,'big')+crc[1].to_bytes(1,'big')
+                txbytes = txbytes + \
+                    crc[0].to_bytes(1, 'big')+crc[1].to_bytes(1, 'big')
                 print(txbytes)
                 connection.send(txbytes)
         if cmd == 3:  # Read holding registers
@@ -81,7 +92,8 @@ def multi_threaded_client(connection):
             if (offs + nreg) > len(registers_holding[id]):
                 txbytes = bytes([id, cmd+0x80, 0x02])
                 crc = modbus_crc16(txbytes)
-                txbytes = txbytes + crc[0].to_bytes(1,'big')+crc[1].to_bytes(1,'big')
+                txbytes = txbytes + \
+                    crc[0].to_bytes(1, 'big')+crc[1].to_bytes(1, 'big')
                 print(txbytes)
                 connection.send(txbytes)
             else:
@@ -89,12 +101,16 @@ def multi_threaded_client(connection):
                 # add the data to the package
                 n = 0
                 while n < nreg:
-                    bytes_val = registers_holding[id][n+offs].to_bytes(2, 'big')
-                    txbytes = txbytes + bytes_val[0].to_bytes(1, 'big') + bytes_val[1].to_bytes(1, 'big')
+                    bytes_val = registers_holding[id][n +
+                                                      offs].to_bytes(2, 'big')
+                    txbytes = txbytes + \
+                        bytes_val[0].to_bytes(
+                            1, 'big') + bytes_val[1].to_bytes(1, 'big')
                     n = n + 1
                 # Add crc to bytes
                 crc = modbus_crc16(txbytes)
-                txbytes = txbytes + crc[0].to_bytes(1, 'big') + crc[1].to_bytes(1, 'big')
+                txbytes = txbytes + \
+                    crc[0].to_bytes(1, 'big') + crc[1].to_bytes(1, 'big')
                 print(txbytes)
                 connection.send(txbytes)
         if cmd == 6:  # Preset single register
@@ -113,12 +129,14 @@ def multi_threaded_client(connection):
                 n = n + 1
             txbytes = bytes([id, cmd, data[2], data[3], data[4], data[5]])
             crc = modbus_crc16(txbytes)
-            txbytes = txbytes + crc[0].to_bytes(1, 'big') + crc[1].to_bytes(1, 'big')
+            txbytes = txbytes + \
+                crc[0].to_bytes(1, 'big') + crc[1].to_bytes(1, 'big')
             print(txbytes)
             connection.send(txbytes)
 
     print("Closing client")
     connection.close()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
